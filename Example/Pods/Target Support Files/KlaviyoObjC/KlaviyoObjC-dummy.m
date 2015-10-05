45a3a1ce970016be218e297a0c65ecd061ec956a
Katherine Keuper <katherinekeuper@Katherines-MacBook-Pro.local>
2015-10-05 14:44:32 -0400

    Initial Commit

---
+++ Example/Pods/Target Support Files/KlaviyoObjC/KlaviyoObjC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KlaviyoObjC : NSObject
+@end
+@implementation PodsDummy_KlaviyoObjC
+@end
---
+++ Example/Pods/Target Support Files/KlaviyoObjC/KlaviyoObjC-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double KlaviyoObjCVersionNumber;
+FOUNDATION_EXPORT const unsigned char KlaviyoObjCVersionString[];
+
